c10c186ec083afbd8d1f18274b74889edb51d3fc
aahluwalia <aahluwalia@middlebury.edu>
2019-11-07 15:15:13 -0500

    ready for feedback

--- self-linker.c
+++ self-linker.c
@@ -8,8 +8,6 @@
 int main(int argc, char * argv[])
 {
   extern void *_GLOBAL_OFFSET_TABLE_;
-  printf("The first entry of the global offset table is %p\n", _GLOBAL_OFFSET_TABLE_);
-  printf("The GOT starts at address %p\n", (void *) &_GLOBAL_OFFSET_TABLE_);
 
   void *handle;
   void *putsFunctionPointer;
@@ -26,19 +24,19 @@ int main(int argc, char * argv[])
   putsFunctionPointer = dlsym(handle, "puts");
   //finds the function pointer from puts using the handler for libc
 
-  void **putsGOT_location = (void*)(&_GLOBAL_OFFSET_TABLE_ + 0x18);
+  void **GOT_locations = &_GLOBAL_OFFSET_TABLE_;
+
+  GOT_locations[3] = putsFunctionPointer;
   //locates where the entry for puts is in the GOT
-  //I found this by running GDB on the program hello-world
-
-  *putsGOT_location = putsFunctionPointer;
   //places the function pointer for puts where it needs to be placed in the GOT
-
-  puts ("hello");
-  puts ("world");
 
   int closeReturnValue = dlclose (handle);
   if (closeReturnValue != 0)
   {
     perror ("Error");
   }
+
+  puts ("hello");
+  puts ("world");
+
 }
